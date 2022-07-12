04658c3060b5a59c11a5c23eba6386688378f024
ZAKARIA RABHI <61620962+zrabhi@users.noreply.github.com>
2022-07-12 03:44:58 +0100

    almost finished

--- script.js
+++ script.js
@@ -1,87 +1,120 @@
-const game = () => {
-    
-    let playerscore = 0;
-    let computerscore = 0;
-    let moves = 0;    
+// ------------variables---------------
 
-    const playgame = () =>
-    {
-        const Rock = document.querySelector('.Rock');
-        const Paper = document.querySelector('.Paper');
-        const Scissor = document.querySelector('.Scissor');
-        const Items = [Rock,Paper,Scissor];
-        const Computeritems  = ['Rock', 'Paper', 'Scissor'];     
+let playerscore = 0;
+let computerscore = 0;
+let moves = 0;   
+let RockBtn = document.querySelector('.Rock');
+let PaperBtn = document.querySelector('.Paper');
+let  ScissorBtn = document.querySelector('.Scissor');
+let Computeritems  = ['Rock','Paper','Scissor'];     
+let Items = [RockBtn,PaperBtn,ScissorBtn];
+let movesLeft = document.querySelector('.movesleft');
 
-        Playerchoices.forEach(choices => {
-            choices.addEventListner('click', function(){
-                const movesLeft = document.querySelector('.movesleft');
+// ---------------functions------------------
+
+function playGame() {
+        
+        Items.forEach(choices => {
+            choices.addEventListener('click', function(){
+                
                 moves++;
-                movesLeft.innerText = 'Moves Left : ${5-moves}';
-            
+                movesLeft.innerText = `Moves Left: ${5-moves}`;
+                
                 const c_choice = Math.floor(Math.random()*3);
                 const Computerchoice = Computeritems[c_choice]; 
                 winner(this.innerText, Computerchoice);
-    
+                
                 if (moves == 5){
                     gameOver(Items, movesLeft);
                 }
-            })        
+            })
         })  
     }
-
     const winner = (Player, Computer) => 
     {
-        const result = document-querySelector('.result');
+        const result = document.querySelector('.result');
         const playerCount = document.querySelector('.p-count');
         const computerCount = document.querySelector('.c-count'); 
-
+        
         Player = Player.toLowerCase();
         Computer = Computer.toLowerCase();
-
+        
         if (Player == Computer)
-            result.innerText = 'It s a tie'
-        else if (Player == 'Rock' && Computer == 'Paper')
-            {
-                result.textContent = 'Computer Won this round';
-                computerscore++;
-                computerCount.textContent = computerscore;
-            }
-        else if (Player == 'Rock' && Computer == 'Scissor')
-            {
-                result.textContent = 'Player Won this round';
-                playerscore++;
-                playerCount.textContent = playerscore;
-            }
-        else if (Player == 'Scissor' && Computer == 'Rock')
-            {
-                result.textContent = 'Computer won this round';
-                computerscore++;
-                computerCount.textContent = computerscore; 
-            }
-        else if (Player == 'Scissor' && Computer == 'Paper')
-            {
-                result.text.Content = 'Player won this round';
-                playerscore++;
-                playerCount.textContent = playerscore; 
-            }
-        else if (Player == 'Paper' && computer == 'Scissor')
-            {
-                result.textContent = 'Computer won this round';
-                computerscore++;
-                computerCount.textContent = computerscore;
-            }
-        else if (Player == 'Paper' && Computer == 'Rock')
+        result.innerText = 'It s a tie'
+        else if (Player == 'rock' && Computer == 'paper')
         {
-            result.text.Content = 'Player won this round';
+            result.innerText = 'Computer Won this round';
+            computerscore++;
+            computerCount.innerText = `${computerscore}`;
+        }
+        else if (Player == 'rock' && Computer == 'scissor')
+        {
+            result.innerText = 'Player Won this round';
             playerscore++;
-            playerCount.textContent = playerscore; 
+            playerCount.innerText = `${playerscore}`;
+        }
+        else if (Player == 'scissor' && Computer == 'rock')
+        {
+            result.innerText = 'Computer won this round';
+            computerscore++;
+            computerCount.innerText = `${computerscore}`; 
+        }
+        else if (Player == 'scissor' && Computer == 'paper')
+        {
+            result.innerText = 'Player won this round';
+            playerscore++;
+            playerCount.innerText = `${computerscore}`; 
+        }
+        else if (Player == 'paper' && Computer == 'scissor')
+        {
+            result.innerText = 'Computer won this round';
+            computerscore++;
+            computerCount.innerText = `${computerscore}`;
+        }
+        else if (Player == 'paper' && Computer == 'rock')
+        {
+            result.textContent = 'Player won this round';
+            playerscore++;
+            playerCount.innerText = `${playerscore}`; 
         }
     }
-    const gameover = (pCount, cCount) => {
-
-
-    
-    } 
-    playgame();
-}
-game();
+    const gameOver = (pCount, cCount) => {
+        
+        const chooseMove = document.querySelector('.move');
+        const result = document.querySelector('.result');
+        const reloadBtn = document.querySelector('.reload');
+        
+        Items.forEach(choices => {
+            choices.style.display = 'none';
+        })
+        
+        chooseMove.innerText = 'Game over!';
+        movesLeft.style.display = 'none';
+        
+        // if (movesLeft == 0) {
+        //     console.log("hey");
+            reloadBtn.innerText = 'Play again';
+            reloadBtn.style.display = 'flex';
+            reloadBtn.style.textAlign = "center";
+            reloadBtn.addEventListener('click', function() {
+                window.location.reload();
+            })        
+        // }
+        if (playerscore == computerscore){
+            result.style.display = '2rem';
+            result.innerText = 'Its a tie, no one won this round';
+            result.style.color = '#308D46';
+        }
+        else if (playerscore > computerscore){
+            result.style.fontSize = '2rem';
+            result.innerText = 'YOU WON THIS GAME!';
+            result.style.color= '#308D46';
+        }
+        else if( playerscore < computerscore){
+            result.style.fontSize = '2rem';
+            result.innerText = 'SADLY YOU LOST ):'
+            result.style.color = '#308D46';
+        }
+    }
+    playGame();
+    // game();
